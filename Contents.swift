5dd273ea22f5533919e6185e4cf2a3ae7a1571d1
Baiba <79092623+BaibaVaisle@users.noreply.github.com>
2021-07-23 01:30:17 +0300

    Initial commit

---
+++ Contents.swift
@@ -0,0 +1,86 @@
+import UIKit
+
+/*
+ Exercise 1
+ Create enum CalculationType as String, where will be: addition, subtraction, multiplication, division with String value
+ */
+/*
+ Exercise 1.1
+ Create a func calculateResult with argument firstNumber, parametrs numberOne, data type Int, + argument andSecondNumber, parametrs numberTwo, data type Int ,+ argument withCalculationType, parametrs calculationType, data type CalculationType and everything will return Int.
+ Than use switch case to calculate and return Int result
+    var result = numberOne
+       switch calculationType {
+       case .addition: result += numberTwo
+        .....
+       }
+  print("Result:  \(calculationType.rawValue) \(numberOne) and \(numberTwo) = \(result)")
+    return result
+ */
+/*
+Exercise 1.2
+Declare two numbers.
+Call func 4 times for all calculateResult
+*/
+
+enum CalculationType: String {
+    case addition = "addition"
+    case subtraction = "subtraction"
+    case multiplication = "multiplication"
+    case division = "division"
+}
+func calculateResult(firstNumber numberOne: Int, andSecondNumber numberTwo: Int, withCalculationType calculationType: CalculationType
+    ) -> Int {
+    var result = numberOne
+    switch calculationType {
+    case .addition:
+        result += numberTwo
+    case .subtraction: result -= numberTwo
+    case .multiplication: result *= numberTwo
+//    case .division: numberTwo != 0 ? result /= numberTwo : print("No divide to 0")
+    case .division:
+        guard numberTwo > 0 else {
+            print ("It is not allowed to devide by 0.")
+            return result
+        }
+        result /= numberTwo
+    }
+    if result != 0{
+        print("Result:  \(calculationType.rawValue) \(numberOne) and \(numberTwo) = \(result)")
+    }
+    return result
+}
+let numberOne = 10
+let numberTwo = 0
+calculateResult(firstNumber: numberOne, andSecondNumber: numberTwo, withCalculationType: .addition)
+calculateResult(firstNumber: numberOne, andSecondNumber: numberTwo, withCalculationType: .division)
+calculateResult(firstNumber: numberOne, andSecondNumber: numberTwo, withCalculationType: .multiplication)
+calculateResult(firstNumber: numberOne, andSecondNumber: numberTwo, withCalculationType: .subtraction)
+/*
+Exercise 2.1
+Make example of struct Car with the name audiQ7
+Make a copy of audiQ7 and name it audiTT.
+Provide the correct name for audiTT
+Than run getSpecs() for audiQ7 and audiTT
+ */
+
+/*
+Exercise 2
+Create struct Car with elements
+    name: String
+    productionYear: Int
+    horsPower: Int
+ Create func getSpecs() print it with elements provided above
+ */
+struct Car {
+  var name: String
+  var productionYear: Int
+  var horsPower: Int
+  func getSpecs(){
+    print("\(name)", "\(productionYear)", "\(horsPower)" )
+  }
+}
+let audiQ7 = Car(name: "audiQ7", productionYear: 2020, horsPower: 1500)
+var audiTT = audiQ7
+audiTT.name = "audiTT"
+audiQ7.getSpecs()
+audiTT.getSpecs()
